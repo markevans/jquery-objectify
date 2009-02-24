6799c06b0bb0818936102bd8015b53e007c5fe1f
Mark Evans <mark@new-bamboo.co.uk>
2009-02-24 15:45:50 +0000

    Created helper for making objectified plugins. - doesn't yet expose via public event interface

---
+++ jquery-ext.js
@@ -0,0 +1,54 @@
+(function(){
+  
+  // objectifyPlugin defines a new jquery 'plugin' method using the name passed in
+  //
+  // e.g. $.objectifyPlugin('hello', ...) is equivalent to
+  //      $.fn.hello = function(){ ... }
+  //
+  // However it also defines a class (i.e. function), and when $('some_selector').hello() is called,
+  // instantiates objects of that class and 'attaches' one to each of the selected elements
+  //
+  // The arguments passed to this method define the behaviour of the class, as in this example:
+  //
+  // $.objectifyPlugin('hello',{
+  //   instance: { colour: red },                                  // instance methods/attributes of the class
+  //   prototype: { display: function(){ alert('hello'); } },      // prototype methods/attributes of the class
+  //   init: function(i, obj, elem){ ...do stuff as you would with a normal plugin...}      // what the plugin actually does
+  // })
+  //
+  //
+  $.objectifyPlugin = function(name, opts){
+    
+    // '..._vars' correspond to both methods and attributes
+    
+    var instance_vars  = opts.instance  || {};
+    var prototype_vars = opts.prototype || {};
+    var code_block     = opts.init      || function(){};
+
+    // Define class with prototype methods and attributes
+    var klass = function(){};
+    $.extend(klass.prototype, prototype_vars);
+
+    // Define the plugin
+    $.fn[name] = function(){
+    
+      $(this).each(function(i){
+        var elem = this;
+        var obj  = new klass();
+        
+        // Instantiate the object and add instance methods/attributes to it
+        var obj = new klass();
+        $.extend(obj, instance_vars);
+        
+        obj.elem = elem; // Make the elem available for use in methods via 'this.elem'
+
+        code_block(elem, obj, i);
+
+      });
+    
+      return $(this);
+    }
+  
+  }
+
+})(jQuery);
--- lib.js
+++ lib.js
@@ -2,47 +2,34 @@
 
 //////// Square ////////
 
-var Square = function(dom_elem){
-  this.dom_elem = dom_elem;
-  this.i = 0;
-  this.counter_interval_id = 0;
-};
-Square.prototype = {
-  
-  nextCount: function(){
-    this.i++
-    $(this.dom_elem).html(this.i);
-    if(this.i == 5){
-      clearInterval(this.counter_interval_id);
-      $().trigger('square.finished_counting', [this]);
+$.objectifyPlugin('square', {
+  instance: {
+    count: 0,
+    counter_interval_id: 0
+  },
+  prototype: {
+    colourBlue: function(){
+      $(this.elem).css({'background-color': 'blue'});
+    },
+    nextCount: function(){
+      this.count++
+      $(this.elem).html(this.count);
+      if(this.count == 5){
+        clearInterval(this.counter_interval_id);
+        $().trigger('square.finished_counting', [this]);
+      }
+    },
+    countToFive: function(){
+      $().trigger('square.starting_to_count', [this]);
+      var self = this;
+      this.counter_interval_id = setInterval(function(){ self.nextCount() }, 1000);
     }
   },
-  
-  countToFive: function(){
-    $().trigger('square.starting_to_count', [this]);
-    var self = this;
-    this.counter_interval_id = setInterval(function(){ self.nextCount() }, 1000);
-  },
-  
-  colourBlue: function(){
-    $(this.dom_elem).css({'background-color': 'blue'});
-  },
-
-};
-$.fn.square = function(){
-  $(this).each(function(i){
-    
-    // Do stuff
-    var square = new Square(this);
-    square.num = i;
-    
-    // Public interface
-    $(this).bind('click',       function(){ square.countToFive() })
-           .bind('colour_blue', function(){ square.colourBlue()  })
-
-  });
-}
-
+  init: function(elem, obj, i){
+    obj.num = i;
+    $(elem).click(function(){ obj.countToFive() })
+  }
+});
 
 //////// Message Box ////////
 
